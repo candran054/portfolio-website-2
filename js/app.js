7f87c3c913e94daadec31bec3b38073994832c3f
candran054 <candran054@gmail.com>
2023-09-14 11:16:49 +0700

    remove contact me form, replace with whatsapp & telegram with hyperlink

--- js/app.js
+++ js/app.js
@@ -4,13 +4,13 @@ const hamburger_menu = document.querySelector(".hamburger-menu");
 const navbar = document.querySelector("header nav");
 const links = document.querySelectorAll(".links a");
 
-function closeMenu(){
+function closeMenu() {
   navbar.classList.remove("open");
   document.body.classList.remove("stop-scrolling");
 }
 
 hamburger_menu.addEventListener("click", () => {
-  if (!navbar.classList.contains("open")){
+  if (!navbar.classList.contains("open")) {
     navbar.classList.add("open");
     document.body.classList.add("stop-scrolling");
   } else {
@@ -20,40 +20,39 @@ hamburger_menu.addEventListener("click", () => {
 
 links.forEach((link) => link.addEventListener("click", () => closeMenu()));
 
-
 window.addEventListener("scroll", () => {
-    skillsEffect();
-    countUp();
+  skillsEffect();
+  countUp();
+});
+
+function checkScroll(el) {
+  let rect = el.getBoundingClientRect();
+  if (window.innerHeight >= rect.top + el.offsetHeight) return true;
+  return false;
+}
+
+function skillsEffect() {
+  if (!checkScroll(skills_wrap)) return;
+  skills_bars.forEach((skill) => (skill.style.width = skill.dataset.progress));
+}
+
+function countUp() {
+  if (!checkScroll(records_wrap)) return;
+  records_numbers.forEach((numb) => {
+    const updateCount = () => {
+      let currentNum = +numb.innerText;
+      let maxNum = +numb.dataset.num;
+      let speed = 100;
+      const increment = Math.ceil(maxNum / speed);
+
+      if (currentNum < maxNum) {
+        numb.innerText = currentNum + increment;
+        setTimeout(updateCount, 1);
+      } else {
+        numb.innerText = maxNum;
+      }
+    };
+
+    setTimeout(updateCount, 400);
   });
-
-  function checkScroll(el) {
-    let rect = el.getBoundingClientRect();
-    if (window.innerHeight >= rect.top + el.offsetHeight) return true;
-    return false;
-  }
-
-  function skillsEffect() {
-    if (!checkScroll(skills_wrap)) return;
-    skills_bars.forEach((skill) => (skill.style.width = skill.dataset.progress));
-  }
-
-  function countUp() {
-    if (!checkScroll(records_wrap)) return;
-    records_numbers.forEach((numb) => {
-      const updateCount = () => {
-        let currentNum = +numb.innerText;
-        let maxNum = +numb.dataset.num;
-        let speed = 100;
-        const increment = Math.ceil(maxNum / speed);
-  
-        if (currentNum < maxNum) {
-          numb.innerText = currentNum + increment;
-          setTimeout(updateCount, 1);
-        } else {
-          numb.innerText = maxNum;
-        }
-      };
-  
-      setTimeout(updateCount, 400);
-    });
-  }
+}
